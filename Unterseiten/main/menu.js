7de91de9b0ffe431bb6d21479bca2b74315fcf72
FloRiyan1 <87898279+FloRiyan1@users.noreply.github.com>
2023-09-17 21:15:02 +0200

    Oranize

--- Unterseiten/main/menu.js
+++ Unterseiten/main/menu.js
@@ -13,16 +13,37 @@ function arc(x, y, radius, startAngle, endAngle) {
     ctx.arc(x, y, radius, startAngle, endAngle);
 }
 
-// Clear canvas
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawHeart() {
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-// Draw the heart shape
-ctx.fillStyle = "#f73487";
-ctx.beginPath();
-moveTo(150, 100);
-arc(100, 100, 25, 0, Math.PI, false);
-arc(175, 100, 25, 0, Math.PI, false);
-lineTo(100, 200);
-lineTo(200, 200);
-lineTo(150, 100);
-ctx.fill();
+    // Draw the heart shape
+    ctx.fillStyle = "#f73487";
+    ctx.beginPath();
+    moveTo(150, 100);
+    arc(100, 100, 25, 0, Math.PI, false);
+    ctx.stroke();
+
+    setTimeout(() => {
+        arc(175, 100, 25, 0, Math.PI, false);
+        ctx.stroke();
+
+        setTimeout(() => {
+            lineTo(100, 200);
+            ctx.stroke();
+
+            setTimeout(() => {
+                lineTo(200, 200);
+                ctx.stroke();
+
+                setTimeout(() => {
+                    lineTo(150, 100);
+                    ctx.fill();
+                }, 500); // Adjust the delay (in milliseconds) between each step
+            }, 500);
+        }, 500);
+    }, 500);
+}
+
+// Start drawing the heart shape
+drawHeart();
